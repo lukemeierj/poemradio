0db727a36adc3125392c9861be00356b51f81b4c
Luke Meier <lukemeierj@gmail.com>
2016-12-08 00:38:57 -0500

    Attempt to fix markRead issue

--- poemradio/settings.py
+++ poemradio/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'poemradio.wsgi.application'
 
 #DEFAULT -------------------------#
 
-DATABASES = {}
-    """'default': {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
+}
 
 #-------------------------------#
 #
