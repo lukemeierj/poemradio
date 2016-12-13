4705a540a24c62986fc71ae463f8c9e841b9ba92
Luke Meier <lukemeierj@gmail.com>
2016-12-13 00:18:32 -0500

    Fix labels, allow undoing vote

--- poem/forms.py
+++ poem/forms.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 from django import forms
 
 class SubmitPoemForm(forms.ModelForm):
+	
+	title = forms.CharField(label = "", widget=forms.TextInput(attrs={'placeholder': 'title'}))
+	text = forms.CharField(label = "", widget=forms.Textarea(attrs={'placeholder': 'put your poetry in this box'}))
+	source = forms.CharField(label = "", widget=forms.TextInput(attrs={'placeholder': 'Source URL'}))
+
+
 	class Meta:
 		model = Poem
-		widgets = {'tags': forms.CheckboxSelectMultiple()}
+		
 		# fields = ('title', 'text', 'source', 'tags')
+		
 		fields = ('title', 'text', 'source')
--- poemUser/forms.py
+++ poemUser/forms.py
@@ -2,11 +2,17 @@ from .models import PoemUser
 from django.contrib.auth.models import User
 from django import forms
 
+
 class UserForm(forms.ModelForm):
-    username = forms.CharField(max_length=30)
+    username = forms.CharField(max_length=30, label = "", widget = forms.TextInput(attrs={'placeholder': 'username'}))
     class Meta:
         model = User
-        widgets = {'password': forms.PasswordInput()}
+        labels = {'email': "", 'username':"", 'password':"", 'first_name':"", 'last_name':''}
+        widgets = {'password': forms.PasswordInput(attrs={'placeholder': 'password'}),
+                    'email': forms.TextInput(attrs={'placeholder': 'email'}),
+                    'first_name': forms.TextInput(attrs={'placeholder': 'first name'}),
+                    'last_name': forms.TextInput(attrs={'placeholder': 'last name'}),
+                    }
         fields = ('email','username', 'password', 'first_name', 'last_name')
 
 
@@ -20,8 +26,8 @@ class PoemUserForm(forms.ModelForm):
         fields = ('safeMode',)
 
 class LogInForm(forms.Form):
-	username = forms.CharField(max_length = 30)
-	password = forms.CharField(widget=forms.PasswordInput())
+	username = forms.CharField(max_length = 30, label = "", widget=forms.TextInput(attrs={'placeholder': 'username'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'password'}), label = "")
 
 class SubmitPoemForm(forms.ModelForm):
 	pass
