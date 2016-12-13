0404e8eb6ead9ca7bc0898188d5b1a284ce9a4a5
Luke Meier <lukemeierj@gmail.com>
2016-12-13 00:19:01 -0500

    Fix labels, allow undoing vote

--- poemradio/settings.py
+++ poemradio/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY ='d9#25el&e3cv9wrthlyeg5$42*v5$(!z!0_@=!=59sfafab-l6'
-# SECRET_KEY = os.environ['secret_KEY']
+SECRET_KEY = os.environ['secret_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
