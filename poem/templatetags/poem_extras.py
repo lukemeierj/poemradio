f44bef684999407e40de721b1b07964bf67f301d
Luke Meier <lukemeierj@gmail.com>
2017-06-30 19:09:25 -0400

    use mistune markdown

--- poem/templatetags/poem_extras.py
+++ poem/templatetags/poem_extras.py
@@ -2,40 +2,12 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 from django.utils.html import escape
-
+from mistune import markdown
 
 register = template.Library()
-
-
 
 @register.filter(name="markdown", needs_autoescape=True)
 @stringfilter
 def simpleMarkDown(value, autoescape=True):
-    text = escape(value)
-    formatting = ['italic', 'bold', 'bold italic']
-    inFormatted = [False, False, False]
-    special = "*"
-    format = ''
-    output = ''
-    inFormatting = False
-    for char in text:
-        if char not in special and not inFormatting:
-            output += char;
-        elif char not in special and inFormatting:
-            numSpecial = len(format)
-            inFormatting = False
-            format = ''
-            if inFormatted[numSpecial-1]:
-                output += "</span>" 
-            else:
-                output += "<span class = '%s'>" % formatting[numSpecial-1]
-                inFormatted[numSpecial-1] = True
-            output+= char
-
-        elif char in special:
-            format += char
-            inFormatting = True
-    for _ in inFormatted:
-        if _:
-            output += "</span>"
+    output = markdown(value, escape = autoescape, hard_wrap = True)
     return mark_safe(output)
